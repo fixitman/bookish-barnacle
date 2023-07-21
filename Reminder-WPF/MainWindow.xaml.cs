87217e9bb83500464870e0779f04c11a7a9ddaeb
Mike Collins <fixitman.mike@outlook.com>
2023-07-21 10:19:52 -0400

    beginning of add edit window

--- Reminder-WPF/MainWindow.xaml.cs
+++ Reminder-WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using System.Windows.Controls;
 
 namespace Reminder_WPF;
@@ -19,5 +20,30 @@ public partial class MainWindow : Window
     {
         var lv = (ListView)sender;
         VM.SelectedItem = (Reminder)lv.SelectedItem;
+    }
+
+    private void ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if(e.ChangedButton == System.Windows.Input.MouseButton.Left)
+        {
+            var lv = (ListView)sender;
+            Reminder? selected = lv.SelectedItem as Reminder;
+            if(selected != null)
+            {
+                var win1 = new Window1(selected);       
+                win1.Closed += Win1_Closed;
+                win1.Show();
+            }
+        }
+    }
+
+    private void Win1_Closed(object? sender, System.EventArgs e)
+    {
+        var win1 = sender as Window1;
+        if(win1 != null)
+        {
+            var t = win1.Reminder.ReminderText;
+            win1.Closed -= Win1_Closed;              
+        }
     }
 }
