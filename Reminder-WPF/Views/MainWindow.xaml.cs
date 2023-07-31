ac071d60d91ed93296d5763915b1404ce0846cba
Mike Collins <fixitman.mike@outlook.com>
2023-07-31 14:20:24 -0400

    added TestDialog on doubleclick

--- Reminder-WPF/Views/MainWindow.xaml.cs
+++ Reminder-WPF/Views/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Windows.Input;
 using System.Windows.Controls;
 using Reminder_WPF.Models;
 using Reminder_WPF.ViewModels;
+using System.Windows.Data;
 
 namespace Reminder_WPF.Views;
 
@@ -32,9 +33,18 @@ public partial class MainWindow : Window
             Reminder? selected = lv.SelectedItem as Reminder;
             if(selected != null)
             {
-                var win1 = new Window1(selected);       
-                win1.Closed += Win1_Closed;
-                win1.Show();
+                //var win1 = new Window1(selected);       
+                //win1.Closed += Win1_Closed;
+                //win1.Show();
+
+                var dlg = new TestDialog();
+                if (dlg.ShowDialog() == true)
+                {
+                    VM.SelectedItem!.ReminderText = dlg.Reply;
+                    CollectionViewSource.GetDefaultView(VM.Reminders).Refresh();
+                }
+
+
             }
         }
     }
---
+++ Reminder-WPF/Views/TestDialog.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace Reminder_WPF.Views
+{
+   
+    public partial class TestDialog : Window
+    {
+
+        public string Reply { get; set; } = "";
+        public TestDialog()
+        {
+            InitializeComponent();
+            txtReply.Text = Reply;
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            Reply = txtReply.Text;
+            this.DialogResult = true;
+        }
+
+        private void Window_ContentRendered(object sender, System.EventArgs e)
+        {
+            txtReply.SelectAll();
+            txtReply.Focus();
+        }
+    }
+}
