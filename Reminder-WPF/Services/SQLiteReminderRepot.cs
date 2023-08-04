0a0a8becb4f006dafe775667ce40e2cd32456621
Mike Collins <fixitman.mike@outlook.com>
2023-08-04 14:49:23 -0400

    Added SQLiteReminderRepot class

---
+++ Reminder-WPF/Services/SQLiteReminderRepot.cs
@@ -0,0 +1,31 @@
+﻿using Reminder_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reminder_WPF.Services;
+
+public class SQLiteReminderRepot : IDataRepo
+{
+    public SQLiteReminderRepot()
+    {
+
+    }    
+
+    public Task<Reminder> AddReminderAsync(Reminder item)
+    {
+        throw new NotImplementedException();
+    }    
+
+    public Task<bool> DeleteReminderAsync(Reminder item)
+    {
+        throw new NotImplementedException();
+    }    
+
+    public Task<List<Reminder>> GetRemindersAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
