d4effd77d69873bab8a80599044f468528a05a3b
Mike Collins <fixitman.mike@outlook.com>
2023-08-25 11:52:45 -0400

    created DayOfWeek UserControl

---
+++ Reminder-WPF/Views/DoWControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Reminder_WPF.POCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reminder_WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for DoWControl.xaml
+    /// </summary>
+    public partial class DoWControl : UserControl
+    {
+        private DaysOfWeek dow;
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(DoWControl) );
+
+        public DoWControl()
+        {
+            InitializeComponent();
+            dow = new DaysOfWeek();
+            DataContext = dow;
+            
+        }
+
+        public string Text
+        {
+            get => dow.Text; 
+            set => dow.Text = value;
+        }
+        
+    }
+}
