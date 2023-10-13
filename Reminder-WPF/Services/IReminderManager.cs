1a97b5f8ff5a78d4ac0fb6821265d3e1cd987f41
Mike Collins <fixitman.mike@outlook.com>
2023-10-13 09:46:19 -0400

    cleaned up IReminderManager

--- Reminder-WPF/Services/IReminderManager.cs
+++ Reminder-WPF/Services/IReminderManager.cs
@@ -1,18 +1,11 @@
-﻿using Quartz;
-using Reminder_WPF.Models;
-using System.Threading;
+﻿using Reminder_WPF.Models;
 using System.Threading.Tasks;
 
 namespace Reminder_WPF.Services
 {
     public interface IReminderManager
     {
-        string Name { get; }
-
         Task AddReminder(Reminder item);
-        Task JobExecutionVetoed(IJobExecutionContext context, CancellationToken cancellationToken = default);
-        Task JobToBeExecuted(IJobExecutionContext context, CancellationToken cancellationToken = default);
-        Task JobWasExecuted(IJobExecutionContext context, JobExecutionException? jobException, CancellationToken cancellationToken = default);
         Task RemoveReminder(Reminder item);
         Task UpdateReminder(Reminder item);
     }
