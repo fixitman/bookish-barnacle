d4d6d03d13c69439a950cdfc5ef3c9365ca07c87
Mike Collins <fixitman.mike@outlook.com>
2024-03-21 13:01:26 -0400

    fixed update settings

--- Reminder-WPF/AppSettings.Designer.cs
+++ Reminder-WPF/AppSettings.Designer.cs
@@ -49,7 +49,7 @@ namespace Reminder_WPF {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool UpdateSettings {
             get {
                 return ((bool)(this["UpdateSettings"]));
